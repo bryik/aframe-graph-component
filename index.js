3dc112f88f654ac4a02ea4bb795687d3de77002c
Ben Pyrik <ben@wsundine.com>
2016-06-13 20:24:06 -0400

    Add semi-standard linter and format code
    Also added D3 dependency.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+/* global AFRAME, THREE */
 if (typeof AFRAME === 'undefined') {
   throw new Error('Component attempted to register before AFRAME was available.');
 }
 
-var d3 = require("d3")
+var d3 = require('d3');
 
 /**
  * Graph component for A-Frame.
@@ -10,26 +11,26 @@ var d3 = require("d3")
 AFRAME.registerComponent('graph', {
   schema: {
     csv: {
-      type: "string"
+      type: 'string'
     },
     type: {
-      type: "string",
-      default: "scatter"
+      type: 'string',
+      default: 'scatter'
     },
     id: {
-      type: "int",
-      default: "0"
+      type: 'int',
+      default: '0'
     },
     width: {
-      type: "number",
+      type: 'number',
       default: 1
     },
     height: {
-      type: "number",
+      type: 'number',
       default: 1
     },
     depth: {
-      type: "number",
+      type: 'number',
       default: 1
     }
   },
@@ -48,140 +49,140 @@ AFRAME.registerComponent('graph', {
     var depth = data.depth;
 
     // These will be used to set the range of the axes' scales
-    var xRange = [0, width]
-    var yRange = [0, height]
-    var zRange = [0, -depth]
+    var xRange = [0, width];
+    var yRange = [0, height];
+    var zRange = [0, -depth];
 
     // Create graphbox Object3D to hold grids and axis labels
     var graphbox = new THREE.Object3D();
 
     // Create graphing area out of three textured planes
     var grid = gridMaker(width, height, depth);
-    graphbox.add( grid );
+    graphbox.add(grid);
 
     // Label using sprites
     // using the same padding for all axes does not work very well...magic numbers for now
-    var xLabel = spriteMaker("x");
+    var xLabel = spriteMaker('x');
     xLabel.position.z = (depth / 2) + 0.01;
     xLabel.position.y = -0.1;
-    graphbox.add( xLabel );
+    graphbox.add(xLabel);
 
-    var yLabel = spriteMaker("y");
+    var yLabel = spriteMaker('y');
     yLabel.position.x = (width / 2) + 0.15;
     yLabel.position.z = -(depth / 2) - 0.1;
     yLabel.position.y = (height / 2);
-    graphbox.add( yLabel );
+    graphbox.add(yLabel);
 
-    var zLabel = spriteMaker("z");
+    var zLabel = spriteMaker('z');
     zLabel.position.x = (width / 2) + 0.15;
     zLabel.position.y = -0.1;
-    graphbox.add( zLabel );
+    graphbox.add(zLabel);
 
     // Add completed graphbox to element's Object3D
-    object3D.add( graphbox );
+    object3D.add(graphbox);
 
     /**
      * Create origin point.
      * This gives a solid reference point for scaling data.
      * It is positioned at the vertex of the left grid and bottom grid (towards the front).
      */
-    var originPointPosition = (-width / 2) + " 0 " + (depth / 2);
-    var originPointID = "originPoint" + data.id;
+    var originPointPosition = (-width / 2) + ' 0 ' + (depth / 2);
+    var originPointID = 'originPoint' + data.id;
 
-    d3.select(el).append("a-entity")
-                 .attr("id", originPointID)
-                 .attr("position", originPointPosition)
-                 // DEBUG
-                 //.attr("geometry", "primitive: sphere; radius: 0.021")
-                 //.attr("material", "color: green");
+    d3.select(el).append('a-entity')
+                 .attr('id', originPointID)
+                 .attr('position', originPointPosition);
+                 /** DEBUG
+                  * .attr('geometry', "primitive: sphere; radius: 0.021")
+                  * .attr('material', "color: green");
+                  */
 
-    if ( data.csv ) {
+    if (data.csv) {
       /* Plot data from CSV */
 
-      var originPoint = d3.select("#originPoint" + data.id);
+      var originPoint = d3.select('#originPoint' + data.id);
 
       // Convert CSV data from string to number
-      d3.csv(data.csv, function(data) {
-        data.forEach(function(d) {
+      d3.csv(data.csv, function (data) {
+        data.forEach(function (d) {
           d.x = +d.x;
           d.y = +d.y;
           d.z = +d.z;
         });
 
-        plotData(data)
+        plotData(data);
       });
 
-      function plotData(data) {
+      var plotData = function (data) {
         // Scale x, y, and z values
         // d3.extent is just short hand for d3.max and d3.min.
-        var xExtent = d3.extent(data, function(d) { return d.x });
+        var xExtent = d3.extent(data, function (d) { return d.x; });
         var xScale = d3.scale.linear()
                        .domain(xExtent)
                        .range([xRange[0], xRange[1]]);
 
-        var yExtent = d3.extent(data, function(d) { return d.y });
+        var yExtent = d3.extent(data, function (d) { return d.y; });
         var yScale = d3.scale.linear()
                        .domain(yExtent)
                        .range([yRange[0], yRange[1]]);
 
-        var zExtent = d3.extent(data, function(d) { return d.z });
+        var zExtent = d3.extent(data, function (d) { return d.z; });
         var zScale = d3.scale.linear()
                        .domain(zExtent)
                        .range([zRange[0], zRange[1]]);
 
         // Append data to graph and attach event listeners
-        originPoint.selectAll("a-sphere")
+        originPoint.selectAll('a-sphere')
                    .data(data)
                    .enter()
-                   .append("a-sphere")
-                   .attr("radius", 0.02)
-                   .attr("color", "#D50000")
-                   .attr("position", function(d) {
-                     return xScale(d.x) + " " + yScale(d.y) + " " + zScale(d.z);
+                   .append('a-sphere')
+                   .attr('radius', 0.02)
+                   .attr('color', '#D50000')
+                   .attr('position', function (d) {
+                     return xScale(d.x) + ' ' + yScale(d.y) + ' ' + zScale(d.z);
                    })
-                   .on("mouseenter", mouseEnter)
-                   .on("mouseleave", mouseLeave);
+                   .on('mouseenter', mouseEnter)
+                   .on('mouseleave', mouseLeave);
 
         /**
          * Event listeners add and remove data labels.
          * "this" refers to sphere element of a given data point.
          */
-        function mouseEnter() {
+        function mouseEnter () {
           // Retrieve original data
           var dataValues = this.__data__;
 
           // Give label object a name so it can be removed later
           var label = new THREE.Object3D();
-          label.name = "tempDataLabel";
+          label.name = 'tempDataLabel';
 
           // Create individual x, y, and z labels using original data values
           // round to 1 decimal space (should use d3 format for consistency later)
-          var xLabel = spriteMaker( d3.round(dataValues.x, 1) + "," );
-          label.add( xLabel );
+          var xLabel = spriteMaker(d3.round(dataValues.x, 1) + ',');
+          label.add(xLabel);
 
-          var yLabel = spriteMaker( d3.round(dataValues.y, 1) + "," );
+          var yLabel = spriteMaker(d3.round(dataValues.y, 1) + ',');
           yLabel.position.x = 0.15;
-          label.add( yLabel );
+          label.add(yLabel);
 
-          var zLabel = spriteMaker( d3.round(dataValues.z, 2) );
+          var zLabel = spriteMaker(d3.round(dataValues.z, 2));
           zLabel.position.x = 0.30;
-          label.add( zLabel );
+          label.add(zLabel);
 
           // Position label above and behind data point
           label.position.y = 0.1;
           label.position.z = -0.1;
 
-          this.object3D.add( label );
+          this.object3D.add(label);
         }
 
-        function mouseLeave() {
-
-          var label = this.object3D.getObjectByName("tempDataLabel");
-          this.object3D.remove( label );
+        function mouseLeave () {
+          var label = this.object3D.getObjectByName('tempDataLabel');
+          this.object3D.remove(label);
         }
-      }
+      };
     }
-  },
+  }
 });
 
 /* HELPER FUNCTIONS */
@@ -190,25 +191,25 @@ AFRAME.registerComponent('graph', {
  * planeMaker() creates a plane given width and height (kind of).
  *  It is used by gridMaker().
  */
-function planeMaker(horizontal, vertical) {
+function planeMaker (horizontal, vertical) {
   // Controls texture repeat for U and V
   var uHorizontal = horizontal * 4;
   var vVertical = vertical * 4;
 
   // Load a texture, set wrap mode to repeat
-  var texture = new THREE.TextureLoader().load( "/assets/grid-textures/grid3.png" );
+  var texture = new THREE.TextureLoader().load('/assets/grid-textures/grid3.png');
   texture.wrapS = THREE.RepeatWrapping;
   texture.wrapT = THREE.RepeatWrapping;
   // should be: texture.anisotropy = renderer.getMaxAnisotropy();
   // but I can't figure out how to get this working
   texture.anisotropy = 16;
-  texture.repeat.set( uHorizontal, vVertical );
+  texture.repeat.set(uHorizontal, vVertical);
 
   // Create material and geometry
-  var material = new THREE.MeshBasicMaterial( { map: texture, side: THREE.DoubleSide } );
-  var geometry = new THREE.PlaneGeometry( horizontal, vertical );
+  var material = new THREE.MeshBasicMaterial({map: texture, side: THREE.DoubleSide});
+  var geometry = new THREE.PlaneGeometry(horizontal, vertical);
 
-  return new THREE.Mesh( geometry, material );
+  return new THREE.Mesh(geometry, material);
 }
 
 /**
@@ -218,27 +219,26 @@ function planeMaker(horizontal, vertical) {
  * There are many ways this function could be improved or done differently
  * e.g. buffer geometry, merge geometry, better reuse of material/geometry.
  */
-function gridMaker(width, height, depth) {
-
+function gridMaker (width, height, depth) {
   var grid = new THREE.Object3D();
 
   // AKA bottom grid
-  var xGrid = planeMaker( width, depth );
+  var xGrid = planeMaker(width, depth);
   xGrid.rotation.x = 90 * (Math.PI / 180);
-  grid.add( xGrid );
+  grid.add(xGrid);
 
   // AKA far grid
-  var yPlane = planeMaker( width, height );
+  var yPlane = planeMaker(width, height);
   yPlane.position.y = (0.5) * height;
   yPlane.position.z = (-0.5) * depth;
-  grid.add( yPlane );
+  grid.add(yPlane);
 
   // AKA side grid
-  var zPlane = planeMaker( depth, height )
+  var zPlane = planeMaker(depth, height);
   zPlane.position.x = (-0.5) * width;
   zPlane.position.y = (0.5) * height;
   zPlane.rotation.y = 90 * (Math.PI / 180);
-  grid.add( zPlane );
+  grid.add(zPlane);
 
   return grid;
 }
@@ -248,8 +248,7 @@ function gridMaker(width, height, depth) {
  * Based off the work of Lee Stemkoski and Sue Lockwood.
  * https://bocoup.com/weblog/learning-three-js-with-real-world-challenges-that-have-already-been-solved
  */
-function spriteMaker(message) {
-
+function spriteMaker (message) {
   // Create canvas, load font and size
   var canvas = document.createElement('canvas');
   var context = canvas.getContext('2d');
@@ -272,12 +271,12 @@ function spriteMaker(message) {
   context.fillText(message, 0, ypos);
 
   // canvas contents will be used for texture
-  var texture = new THREE.Texture(canvas)
-      texture.minFilter = THREE.LinearFilter;
-      texture.needsUpdate = true;
+  var texture = new THREE.Texture(canvas);
+  texture.minFilter = THREE.LinearFilter;
+  texture.needsUpdate = true;
 
   var spriteMaterial = new THREE.SpriteMaterial({ map: texture });
-  var sprite = new THREE.Sprite( spriteMaterial );
-  sprite.scale.set(0.125,0.125,0.125);
+  var sprite = new THREE.Sprite(spriteMaterial);
+  sprite.scale.set(0.125, 0.125, 0.125);
   return sprite;
 }
